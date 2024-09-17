985f1d95719fb0fbc0e9c653895566e90801cea3
Denis Humeniuk <mail05denis@gmail.com>
2024-09-17 22:47:29 +0300

    Finally done lab

--- cp.c
+++ cp.c
@@ -1,18 +1,36 @@
 #include "types.h"
 #include "user.h"
 #include "fcntl.h"
+#include "stat.h"
+
+int is_directory(struct stat *st) {
+    return st->type == T_DIR;
+}
 
 void cp(char *from, char *to) {
     int fd1, fd2, n;
     char buf[512];
 
-    if((fd1 = open(from, O_RDONLY)) < 0) {
-        printf(1, "cp: cannot open %s\n", from);
+    struct stat st_1;
+
+    if (stat(from, &st_1) < 0){
+        printf(1, "Error in source file\n");
+    }
+
+    if (is_directory(&st_1)) {
+        printf(1, "Can not copy from directory\n");
+    }
+
+    fd1 = open(from, O_RDONLY);
+    fd2 = open(to, O_CREATE | O_WRONLY);
+
+    if(fd1 < 0) {
+        printf(1, "Cannot open source file\n");
         exit();
     }
 
-    if((fd2 = open(to, O_CREATE | O_WRONLY)) < 0) {
-        printf(1, "cp: cannot open %s\n", to);
+    if(fd2 < 0) {
+        printf(1, "Cannot open destination file\n");
         exit();
     }
 
@@ -32,4 +50,3 @@ int main(int argc, char *argv[]) {
     cp(argv[1], argv[2]);
     exit();
 }
-
--- mv.c
+++ mv.c
@@ -6,14 +6,16 @@ int main(int argc, char *argv[]) {
         printf(1, "Usage: mv <from_file> <to_file>\n");
         exit();
     }
+    
     if(link(argv[1], argv[2]) < 0) {
-        printf(1, "mv: cannot link %s to %s\n", argv[1], argv[2]);
+        printf(1, "Cannot link first path to second\n");
         exit();
     }
+
     if(unlink(argv[1]) < 0) {
-        printf(1, "mv: cannot unlink %s\n", argv[1]);
+        printf(1, "Cannot unlink file from source path\n");
         exit();
     }
+
     exit();
 }
-
